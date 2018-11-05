1695cdcd3d0657de7a756bc8db17f0e9ed8331c0
alyssa_bartuch <alyssabartuch@gmail.com>
2018-11-05 07:42:18 -0500

    Spread operator on objects

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -4,6 +4,8 @@ const printTeam = (team, coach, ...players) => {
    console.log(`Players: ${players.join(', ')}`);
 }
 
+// printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+
 const team = {
    name: 'Liberty',
    coach: 'Casey Penn',
@@ -13,8 +15,42 @@ const team = {
 // Spread Operator
 printTeam(team.name, team.coach, ...team.players);
 
+
 let cities = ['barcelona', 'cape town', 'bordeaux'];
 cities = [...cities, 'santiago'];
 
 // citiesCopy.push('santiago');
 console.log(cities);
+
+let house = {
+   bedrooms: 2,
+   bathrooms: 1.5,
+   yearBuilt: 2017
+}
+
+let newHouse = {
+   basement: true,
+   ...house,
+   bedrooms: 3
+}
+
+console.log(house);
+console.log(newHouse);
+
+
+let person = {
+   name: 'alyssa',
+   age: 27
+}
+
+let location = {
+   city: 'hudsonville',
+   country: 'US'
+}
+
+let overview = {
+   ...person,
+   ...location
+}
+
+console.log(overview);
